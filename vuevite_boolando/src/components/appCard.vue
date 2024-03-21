d6ee844a85461fcdb28a764d4ab27399dd514eb7
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-21 17:16:50 +0100

    edit pèosition for images

--- vuevite_boolando/src/components/appCard.vue
+++ vuevite_boolando/src/components/appCard.vue
@@ -8,7 +8,7 @@ export default {
 <template>
 
     <div class="card col-3 m-3" style="width: 18rem;">
-        <img src="" class="card-img-top" alt="...">
+        <img src="/vuevite_boolando/public/img/1.webp" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
