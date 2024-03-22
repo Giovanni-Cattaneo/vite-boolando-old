bd786ff090dfce3339897b9e155e9bb7abb7056b
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-22 10:35:55 +0100

    turn back from props

--- vuevite_boolando/src/components/appMain.vue
+++ vuevite_boolando/src/components/appMain.vue
@@ -6,69 +6,7 @@ export default {
     name: 'appMain',
     components: {
         appCard
-    },
-    data() {
-        return {
-
-            activeImage: true,
-            activeCard: null,
-            products: [
-                {
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: Number((this.price / 2).toFixed(2)),
-                    image: '../public/1.webp',
-                    altImage: '../public/1b.webp',
-                    originalImage: '../public/1.webp',
-                },
-                {
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/2.webp',
-                    altImage: '../public/2b.webp',
-                    originalImage: '../public/2.webp',
-                },
-                {
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/3.webp',
-                    altImage: '../public/3b.webp',
-                    originalImage: '../public/3.webp',
-                },
-                {
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/4.webp',
-                    altImage: '../public/4b.webp',
-                    originalImage: '../public/4.webp',
-                },
-                {
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/5.webp',
-                    altImage: '../public/5b.webp',
-                    originalImage: '../public/5.webp',
-                },
-                {
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/6.webp',
-                    altImage: '../public/6b.webp',
-                    originalImage: '../public/6.webp',
-                },
-            ]
-        }
     }
-
 }
 
 </script>
@@ -77,11 +15,7 @@ export default {
 
     <div class="container text-center">
         <div class="row">
-            <div class="card col-12 col-lg-3 col-sm-4 m-3 p-0" v-for="(product, index) in products" :key="index"
-                :brand="product.brand" :image="product.image" :altimahe="product.altImage" :price="product.price"
-                :model="product.model" :originalImage="product.originalImage">
-                <appCard />
-            </div>
+            <appCard />
         </div>
     </div>
 
