a9a0d699a27d95b709852f1c93857e22994d9320
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-25 18:24:17 +0100

    move function callApi to state.js

--- vuevite_boolando/src/components/appMain.vue
+++ vuevite_boolando/src/components/appMain.vue
@@ -1,6 +1,5 @@
 <script>
 import appCard from "./appCard.vue";
-import axios from 'axios';
 import { state } from "../state.js"
 
 export default {
@@ -16,29 +15,25 @@ export default {
     },
     methods: {
         updateImage(index) {
-            if (this.products[index].image === this.products[index].originalImage) {
-                this.products[index].image = this.products[index].altImage;
+            if (state.products[index].image === state.products[index].originalImage) {
+                state.products[index].image = state.products[index].altImage;
             } else {
-                this.products[index].image = this.products[index].originalImage
+                state.products[index].image = state.products[index].originalImage
             }
         },
-        callApi() {
-            axios.get('http://localhost:3000/products').then(response => {
-                console.log(response)
-                this.products = response.data
-            })
-        }
+
     },
     mounted() {
-        this.callApi();
+        state.callApi()
     }
+
 }
 </script>
 
 <template>
     <div class="container text-center">
         <div class="row">
-            <div class="card col-12 col-lg-3 col-sm-4 m-3 p-0" v-for="(product, index) in this.products" :key="index">
+            <div class="card col-12 col-lg-3 col-sm-4 m-3 p-0" v-for="(product, index) in state.products" :key="index">
                 <appCard :brand="product.brand" :image="product.image" :price="product.price" :model="product.model"
                     :altImage="product.altImage" :originalImage="product.originalImage" :altPrice="product.altPrice"
                     :index="index" @click="updateImage(index)" />
