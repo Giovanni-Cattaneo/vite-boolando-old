5e4312eaa1eb64ec896040f60450ce470a36ca2c
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-25 22:38:02 +0100

    Revert "move function callApi to state.js"
    This reverts commit a9a0d699a27d95b709852f1c93857e22994d9320.

--- vuevite_boolando/src/components/appMain.vue
+++ vuevite_boolando/src/components/appMain.vue
@@ -1,5 +1,6 @@
 <script>
 import appCard from "./appCard.vue";
+import axios from 'axios';
 import { state } from "../state.js"
 
 export default {
@@ -15,25 +16,29 @@ export default {
     },
     methods: {
         updateImage(index) {
-            if (state.products[index].image === state.products[index].originalImage) {
-                state.products[index].image = state.products[index].altImage;
+            if (this.products[index].image === this.products[index].originalImage) {
+                this.products[index].image = this.products[index].altImage;
             } else {
-                state.products[index].image = state.products[index].originalImage
+                this.products[index].image = this.products[index].originalImage
             }
         },
-
+        callApi() {
+            axios.get('http://localhost:3000/products').then(response => {
+                console.log(response)
+                this.products = response.data
+            })
+        }
     },
     mounted() {
-        state.callApi()
+        this.callApi();
     }
-
 }
 </script>
 
 <template>
     <div class="container text-center">
         <div class="row">
-            <div class="card col-12 col-lg-3 col-sm-4 m-3 p-0" v-for="(product, index) in state.products" :key="index">
+            <div class="card col-12 col-lg-3 col-sm-4 m-3 p-0" v-for="(product, index) in this.products" :key="index">
                 <appCard :brand="product.brand" :image="product.image" :price="product.price" :model="product.model"
                     :altImage="product.altImage" :originalImage="product.originalImage" :altPrice="product.altPrice"
                     :index="index" @click="updateImage(index)" />
--- vuevite_boolando/src/state.js
+++ vuevite_boolando/src/state.js
@@ -1,15 +1,8 @@
 import { reactive } from 'vue';
-import axios from 'axios';
+
 
 export const state = reactive({
     message: "This is a test",
     products: [],
     api_product_url: 'http://localhost:3000/products',
-    callApi() {
-        axios.get(this.api_product_url).then(response => {
-            console.log(response)
-            state.products = response.data
-        })
-    },
-
 })
