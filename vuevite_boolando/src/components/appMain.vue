45b6f6245dc4054b4202dbe7ea67bd0aa9d4213e
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-03-25 16:37:17 +0100

    add callApi

--- vuevite_boolando/src/components/appMain.vue
+++ vuevite_boolando/src/components/appMain.vue
@@ -1,5 +1,4 @@
 <script>
-
 import appCard from "./appCard.vue";
 import axios from 'axios';
 import { state } from "../state.js"
@@ -11,68 +10,7 @@ export default {
     },
     data() {
         return {
-            products: [
-                {
-                    id: 1,
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: Number((this.price / 2).toFixed(2)),
-                    image: '../public/1.webp',
-                    altImage: '../public/1b.webp',
-                    originalImage: '../public/1.webp',
-                },
-                {
-                    id: 2,
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/2.webp',
-                    altImage: '../public/2b.webp',
-                    originalImage: '../public/2.webp',
-                },
-                {
-                    id: 3,
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/3.webp',
-                    altImage: '../public/3b.webp',
-                    originalImage: '../public/3.webp',
-                },
-                {
-                    id: 4,
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/4.webp',
-                    altImage: '../public/4b.webp',
-                    originalImage: '../public/4.webp',
-                },
-                {
-                    id: 5,
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/5.webp',
-                    altImage: '../public/5b.webp',
-                    originalImage: '../public/5.webp',
-                },
-                {
-                    id: 6,
-                    brand: 'Levis',
-                    model: 'RELAXED FEET TEE UNISEX',
-                    price: 14.99,
-                    altPrice: parseInt((this.price / 2).toFixed(2)),
-                    image: '../public/6.webp',
-                    altImage: '../public/6b.webp',
-                    originalImage: '../public/6.webp',
-                },
-            ],
+            products: [],
             state: state,
         }
     },
@@ -83,24 +21,24 @@ export default {
             } else {
                 this.products[index].image = this.products[index].originalImage
             }
-
+        },
+        callApi() {
+            axios.get('http://localhost:3000/products').then(response => {
+                console.log(response)
+                this.products = response.data
+            })
         }
     },
     mounted() {
-        console.log(state.message);
-        axios.get('http://localhost:3000/products').then(response => {
-            console.log(response)
-            this.products = response.data
-        })
+        this.callApi();
     }
 }
-
 </script>
 
 <template>
     <div class="container text-center">
         <div class="row">
-            <div class="card col-12 col-lg-3 col-sm-4 m-3 p-0" v-for="(product, index) in products" :key="index">
+            <div class="card col-12 col-lg-3 col-sm-4 m-3 p-0" v-for="(product, index) in this.products" :key="index">
                 <appCard :brand="product.brand" :image="product.image" :price="product.price" :model="product.model"
                     :altImage="product.altImage" :originalImage="product.originalImage" :altPrice="product.altPrice"
                     :index="index" @click="updateImage(index)" />
